8df94f677b42a32435a92bf1bd4051ccc1934b7e
Iryna Samusevich <Vladislavovna2018@bk.ru>
2024-12-12 20:22:21 +0100

    fix deploy

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 echo "Kill all the running PM2 actions"
-sudo pm2 kill
+pm2 kill
 
 echo "Jump to app folder"
 cd /home/ec2-user/dev/CourseProject
@@ -8,24 +8,24 @@ echo "Update app from Git"
 git pull
 
 echo "Preparing backend to start"
-cd back
+cd /home/ec2-user/dev/CourseProject/back
 echo "Install app dependencies"
-sudo rm -rf node_modules package-lock.json
-sudo npm install
+rm -rf node_modules package-lock.json
+npm install
 
 echo "Run new PM2 action"
-sudo pm2 start --name course-back npm -- start
+pm2 start --name course-back npm -- start
 echo "Backend is ready"
 
-cd ../front
+cd cd /home/ec2-user/dev/CourseProject/front
 echo "Preparing frontend to start"
 cd back
 echo "Install app dependencies"
-sudo rm -rf node_modules package-lock.json
-sudo npm install
+rm -rf node_modules package-lock.json
+npm install
 
 echo "Run new PM2 action"
-sudo pm2 start --name course-front npm -- start
+pm2 start --name course-front npm -- start
 echo "Backend is ready"
 
 echo "Deployment is complete"
