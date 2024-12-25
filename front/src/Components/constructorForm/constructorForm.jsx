46bdf5b0a2b7ee61499befc3443cbb7b363c8014
Iryna Samusevich <Vladislavovna2018@bk.ru>
2024-12-25 15:27:28 +0100

    update survey component

--- front/src/Components/constructorForm/constructorForm.jsx
+++ front/src/Components/constructorForm/constructorForm.jsx
@@ -1,62 +1,44 @@
+import { useState } from "react";
 import { SurveyCreatorComponent, SurveyCreator } from "survey-creator-react";
 import "survey-core/defaultV2.min.css";
 import "survey-creator-core/survey-creator-core.min.css";
-//import './constructorForm.css'
+import './constructorForm.css'
 
 const creatorOptions = {
   showLogicTab: true,
-  isAutoSave: true
-};
-
-const defaultJson = {
-  pages: [{
-    name: "Name",
-    elements: [{
-      name: "FirstName",
-      title: "Enter your first name:",
-      type: "text"
-    }, {
-      name: "LastName",
-      title: "Enter your last name:",
-      type: "text"
-    }]
-  }]
+  isAutoSave: false,
+  showTranslationTab: true,
+  showSidebar: false,
+  //questionTypes: ["text", "checkbox", "radiogroup", "boolean"],
 };
 
 export default function ConstructorForm() {
-  const creator = new SurveyCreator(creatorOptions);
-  creator.text = window.localStorage.getItem("survey-json") || JSON.stringify(defaultJson);
-  creator.saveSurveyFunc = (saveNo, callback) => {
-    window.localStorage.setItem("survey-json", creator.text);
-    callback(saveNo, true);
-    // saveSurveyJson(
-    //     "https://your-web-service.com/",
-    //     creator.JSON,
-    //     saveNo,
-    //     callback
-    // );
-  };
-  // creator.onUploadFile.add((_, options) => {
-  //   const formData = new FormData();
-  //   options.files.forEach(file => {
-  //     formData.append(file.name, file);
-  //   });
-  //   fetch("https://example.com/uploadFiles", {
-  //     method: "post",
-  //     body: formData
-  //   }).then(response => response.json())
-  //     .then(result => {
-  //       options.callback(
-  //         "success",
-  //         // A link to the uploaded file
-  //         "https://example.com/files?name=" + result[options.files[0].name]
-  //       );
-  //     })
-  //     .catch(error => {
-  //       options.callback('error');
-  //     });
-  // });
+  let [creator, setCreator] = useState();
+
+  if (creator === undefined) {
+
+    creator = new SurveyCreator(creatorOptions);
+
+    creator.toolbox.forceCompact = true;
+    // creator.toolbox.changeCategories([
+    //   { name: "text", category: "Panels" },
+    //   { name: "checkbox", category: "Panels" },
+    //   { name: "radiogroup", category: "Panels" },
+    //   { name: "boolean", category: "Panels" },
+    // ]);
+
+    creator.saveSurveyFunc = (no, callback) => {
+      console.log(JSON.stringify(creator.JSON));
+      callback(no, true);
+    };
+
+    setCreator(creator);
+  }
+  //creator.text = window.localStorage.getItem("survey-json") || JSON.stringify(defaultJson);
+
   return (
-    <SurveyCreatorComponent creator={creator} />
+    <div className="App">
+      <SurveyCreatorComponent creator={creator} />
+    </div>
   )
 }
